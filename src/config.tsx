2b8cf76b5e4b3f1d1a2c4e49d7ed63d1423d7cf1
mousebomb <mousebomb@gmail.com>
2023-10-10 08:47:22 +0800

    first clone

---
+++ src/config.tsx
@@ -0,0 +1,15 @@
+export default class Config {
+  //表格名
+  public static TAB_NAME_SJGL: string = "💰商机管理";
+  public static TAB_NAME_TJZB: string = "📋楼宇攻坚报表（提交总部）";
+
+  //商机管理的字段
+  public static FIELD_SJGL_ZJ: string = "支局";
+  public static FIELD_SJGL_KEHU: string = "商机名称";
+  public static FIELD_SJGL_YUJIJINE: string = "预计商机金额";
+  public static FIELD_SJGL_YISHANGBAO: string = "是否已上报";
+
+  //提交总部的字段：
+  public static FIELD_TJZB_KEHU: string = "客户名称";
+
+}
